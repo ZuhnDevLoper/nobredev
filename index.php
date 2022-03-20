86bbc88a78bf1aa01655fe9eb6d40164e5cc434f
ZuhnDevLoper <101981477+ZuhnDevLoper@users.noreply.github.com>
2022-03-20 11:27:00 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<html><title>LUYZ ADM</title>
+    <meta name="theme-color" content="blue">      
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+  <link href="css/signin.css" rel="stylesheet"> <script src="js/ie-emulation-modes-warning.js"></script>
+<center>
+    <font color="red">
+<br>
+<h1> REI DAS TELAS LUYZ FILLYPE </h1>
+<br>
+<H1>ABRIR TELA FAKE </H1>
+	<Form Action="index.html">
+	     <button class="btn btn-success"  type="submit" style="width:300px;">ABRIR TELA  </button></form>
+ <link rel="img" href="img/fu.jpg"> 
+ <audio src="operador/luyzmiusic.mp3" autoplay=""> </audio>
+<H1>ABRIR ADMIN</H1>
+	<Form Action="operador">
+	     <button class="btn btn-success"  type="submit" style="width:300px;">ABRIR ADMIN</button></form>
+
+<style>
+		h5,{
+					font-family:Georgia, "Times New Roman", Times, serif;
+					font-style:italic;
+					font-size:30px;
+					color:#d9524d;
+					letter-spacing:-0.05;
+					text-shadow:2px 1px 1px #666;
+		}
+		body{
+		  background: url('operador/img/luyz-imagens/luyzfundo.jpg') center center no-repeat fixed; 
+		  -webkit-background-size: cover;
+		  -moz-background-size: cover;
+		  -o-background-size: cover;
+		  background-size: cover;
+		}
+		h2 {
+					font-family:Georgia, "Times New Roman", Times, serif;
+					font-style:italic;
+					font-size:30px;
+					color:#28a745;
+					letter-spacing:-0.05;
+					text-shadow:1px 1px 1px 	#000;
+		}
+	img[src*="https://cdn.rawgit.com/000webhost/logo/e9bd13f7/footer-powered-by-000webhost-white2.png"] {display:none;}
+		</style>
+</html>
